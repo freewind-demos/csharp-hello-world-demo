bbedba46f71b3aa9d22133f04bb2a434e6078918
Freewind <nowindlee@gmail.com>
2021-03-28 10:17:32 +0800

    csharp-hello-world-demo

---
+++ csharp-hello-world-demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_hello_world_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
